fe9b1b03a64d9a1911cfb875c7b06187640a087b
rmenegaux <romain.menegaux@gmail.com>
2022-12-03 16:07:53 +0100

    pcq4m scripts

---
+++ pcq4m_train.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -l
+#SBATCH --job-name graph_gps
+#SBATCH -A tbr@v100
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=20
+#SBATCH --hint=nomultithread
+#SBATCH --time=20:00:00
+#SBATCH --output=/gpfswork/rech/tbr/ump88gx/logs/%j.out      # nom du fichier de sortie
+#SBATCH --error=/gpfswork/rech/tbr/ump88gx/logs/%j.out       # nom du fichier d'erreur
+
+
+
+
+module purge
+
+source $HOME/.bashrc
+# module load cuda/10.1.2
+# module load pytorch-gpu/py3/1.6.0
+conda activate $WORK/miniconda3/envs/graphgps
+
+
+cd $WORK/GraphGPS
+
+
+
+config="configs/GPS/pcqm4m-GraphiT+RWSE.yaml"
+echo $config
+python main.py --cfg $config wandb.use True wandb.mode "offline" seed 42
