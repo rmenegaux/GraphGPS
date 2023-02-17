289cefce4afc657886ccde7bce0b2ec6d1c11381
EmmanuelJhno <emmanuel.jehanno@gmail.com>
2023-02-17 16:08:47 +0100

    Running small xp on zinc on oar

--- EJ_extra_zinc.sh
+++ EJ_extra_zinc.sh
@@ -3,4 +3,4 @@ source gpu_setVisibleDevices.sh
 GPUID=0
 cd /home/ejehanno/repos/GraphGPS
 
-python main.py --cfg configs/GPS/zinc-GraphiT_EJ_tests.yaml wandb.name "(QK+E_mono)*(V+E_multi)_DptConn" gt.layer_args "[{'QK_op':'multiplication'}, {'KE_op':'addition'}, {'VE_op':'addition'}, {'dropout_lvl':'connections'}, {'edge_out_dim':1}]" && python main.py --cfg configs/GPS/zinc-GINE+GraphiT_EJ_tests.yaml
+python main.py --cfg configs/GPS/zinc-GraphiT_EJ_tests.yaml
