dc75dae21f0a744b21a22dbc78655e142abc69b8
EmmanuelJhno <emmanuel.jehanno@gmail.com>
2023-03-16 22:55:38 +0100

    d over 4 and sqrt d heads xp

--- main.py
+++ main.py
@@ -56,8 +56,11 @@ def custom_set_out_dir(cfg, cfg_fname, name_tag):
     run_name = os.path.splitext(os.path.basename(cfg_fname))[0]
     run_name += f"-{name_tag}" if name_tag else ""
     cfg.out_dir = os.path.join(os.path.join(cfg.out_dir, run_name), cfg.wandb.name)
-    if not os.path.exists(cfg.out_dir):
+    #if not os.path.exists(cfg.out_dir):
+    try:
         os.makedirs(cfg.out_dir)
+    except:
+        pass
 
 
 def custom_set_run_dir(cfg, run_id):
